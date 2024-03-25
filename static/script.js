fd8296646e99a4f7dc5bed4d9a3e0fe3163c2405
leonardo <leochinelato9852@gmail.com>
2024-03-25 20:02:48 -0300

    Criar site

---
+++ static/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Atualiza o título do modal com o título do card ao exibi-lo
+    var buttons = document.querySelectorAll('.abrir-modal');
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            var cardTitle = this.parentNode.querySelector('.card-title').innerText;
+            var modalTitle = document.querySelector('#myModal .modal-title');
+            modalTitle.innerText = cardTitle;
+        });
+    });
+});
