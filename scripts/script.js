dd15eab0647b1354eaad44adee2ab43680cee298
leonardo <leochinelato9852@gmail.com>
2024-04-03 14:37:50 -0300

    Alterar navbar, media queries e slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Atualiza o título do modal com o título do card ao exibi-lo
-    var buttons = document.querySelectorAll('.abrir-modal');
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function () {
-            var cardTitle = this.querySelector('.card-title').innerText;
-            var modalTitle = document.querySelector('#myModal .modal-title');
-            modalTitle.innerText = cardTitle;
-        });
-    });
-});
+const hambutton = document.querySelector('.hambutton')
+const hamburger = document.querySelector('.hamburger-container')
 
-var prevScrollpos = window.scrollY
-window.onscroll = function () {
-    var currentScrollPos = window.scrollY
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById('navbar').style.top = '0'
-    } else {
-        document.getElementById('navbar').style.top = '-110px'
-    }
-    prevScrollpos = currentScrollPos
-}
-
-const menuHamburger = document.querySelector('.menu-hamburger')
-const navLinks = document.querySelector('.nav-links')
-
-menuHamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('mobile-menu')
+hambutton.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
 })
